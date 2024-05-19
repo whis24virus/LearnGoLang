cdf6a4b337662a2a26bff645fd9f4f654f733ad8
Keshav Mehta <keshavmehta@Keshavs-Laptop.local>
2024-05-19 16:57:45 +0530

    functions done

--- goSecond/main.go
+++ goSecond/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"unicode/utf8"
 )
@@ -14,16 +15,38 @@ func main() {
 	fmt.Println(intNum)
 	var ans float64 = float64(intNum) + float64(24.5)
 	fmt.Println(ans)
-	var stringS string = `hello 
+	var stringS string = `hello
     what are you doing`
 
 	fmt.Println(len(stringS))
 
+	printMe(stringS)
 	//	var charRune rune = 'h'
 	fmt.Println(utf8.RuneCountInString(stringS))
+	fmt.Println(multiply(25, 34))
+	multi, add, err := multiply(26, 25)
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+	fmt.Printf("the multiplication is %v and the addition is %v", multi, add)
 
 	myVar := "hello bootyfull"
 
 	var1, var2 := 24, "3455"
 	fmt.Println(myVar, var1, var2)
+}
+
+func printMe(printValue string) {
+	fmt.Println(printValue)
+}
+
+func multiply(firstNum int, secondNum int) (int, int, error) {
+	var err error
+	if secondNum == 0 {
+		err = errors.New("Second num cannot be zero")
+		return 0, 0, err
+	}
+	var result int = firstNum * secondNum
+	var addition int = firstNum + secondNum
+	return result, addition, err
 }
